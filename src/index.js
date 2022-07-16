eec411a6330f2ed6ecc6366938c9591a0256266e
Felipe <machadofelipe2016@outlook.com>
2022-07-16 15:38:06 -0300

    c4-create more test inside index

--- src/index.js
+++ src/index.js
@@ -3,20 +3,43 @@ import { ApolloServer,gql } from 'apollo-server'
 // here i tick server apollo
 const server = new ApolloServer({
     typeDefs: gql`
-    type  Query{
-        hello:String
-        }
-    
+    type Query {
+        user: User,
+        new : NewUser 
+      }
+     type User {
+      name: String!,
+      id: ID!,
+      telefone: Int
+     }
+
+     type NewUser {
+      email: String!
+      adress: String!    
+      }
+
     `,
-    resolvers:{
-        Query:{
-            hello: () => {
-                "hello again"
-            }
+    resolvers: {
+      Query: {
+        user: () => {
+          return {
+            id:'Programmer',
+            name:"Pmertins",
+            telefone: 3232323
+          }
+        },
+
+        new: () => {
+          return {
+           email:'machadofelipe2016@outlook.com',
+           adress: 'av circular 921'
+          }
         }
+      } 
     }
-})
+    
+});
 
 server.listen(4003).then(({url}) => {
-    console.log(`server listening here ${url} `)
+    console.log(`server listening here ${url}`)
 })
