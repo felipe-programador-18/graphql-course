c34b1f68c89f124d0c3137ff7fb2a85727dd07ca
Felipe <machadofelipe2016@outlook.com>
2022-07-17 11:43:46 -0300

    c13-adding more object to complete date

--- src/graph/post/anotheresolvers.js
+++ src/graph/post/anotheresolvers.js
@@ -1,14 +1,55 @@
 
-const brand = () => {
+const post = () => {
     return {
-        namebrand: 'renault',
-        years:2002
+        id:'21',
+        title:"create machine another package of create bottle",
+        body:"fdsafdsaf",
+        indexRef: 323,
+        createdAt:"2020102-fdsafdsf",
     }
 
 }
 
+const posts = () => {
+    return [{
+        id:'21',
+        title:"create machine learning",
+        body:"fdafd",
+        indexRef: 323,
+        createdAt:"2020102-fdsafdsf"
+    },{
+        id:'213',
+        title:"machine learning",
+        body:"fdafd",
+        indexRef: 323,
+        createdAt:"2020102-fdsafdsf"
+    }, {
+        id:'21',
+        title:"create machine another package of create bottle",
+        body:"fdafd",
+        indexRef: 323,
+        createdAt:"2020102-fdsafdsf",
+    },
+    
+    {
+        id:'21',
+        title:"create machine another package of create bottle",
+        body:"fdaf",
+        indexRef: 323,
+        createdAt:"2020102-fdsafdsf",}
+
+
+
+
+
+
+]
+}
+
+
 export const useAnotherSolve = {
-    Query :{
-        brand
+    Query:{
+        post,
+        posts
     }
 }
