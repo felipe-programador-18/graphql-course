1d57b6deb217e9af2c37dfdb7e694ee985ef7097
Felipe <machadofelipe2016@outlook.com>
2022-07-17 12:54:49 -0300

    c16-back use this index

--- src/index.js
+++ src/index.js
@@ -1,44 +1,11 @@
 import { ApolloServer,gql } from 'apollo-server'
-
+import { resolvers, typeDefs } from './graph/schema'
 // here i tick server apollo
 const server = new ApolloServer({
-    typeDefs: gql`
-    type Query {
-        user: User,
-        new : NewUser 
-      }
-     type User {
-      name: String!,
-      id: ID!,
-      telefone: Int
-     }
+  typeDefs,
+  resolvers,
 
-     type NewUser {
-      email: String!
-      adress: String!    
-      }
-
-    `,
-    resolvers: {
-      Query: {
-        user: () => {
-          return {
-            id:'Programmer',
-            name:"Pmertins",
-            telefone: 3232323
-          }
-        },
-
-        new: () => {
-          return {
-           email:'machadofelipe2016@outlook.com',
-           adress: 'av circular 921'
-          }
-        }
-      } 
-    }
-    
-});
+})
 
 server.listen(4003).then(({url}) => {
     console.log(`server listening here ${url}`)
