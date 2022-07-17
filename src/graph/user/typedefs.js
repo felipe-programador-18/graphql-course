c31db9a4dbc478da55706386e4a82647c926fcf4
Felipe <machadofelipe2016@outlook.com>
2022-07-17 11:42:55 -0300

    c11- trade and adding more typeof date

--- src/graph/user/typedefs.js
+++ src/graph/user/typedefs.js
@@ -4,6 +4,9 @@ import {gql } from "apollo-server"
 //tips very interest, i need have only one query in my project!!
 // if me want more of one query i need use extend to extends my query!!
 
+
+
+
 export const useTypeDefs = gql`
   extend type Query {
     user:User!,
@@ -12,6 +15,11 @@ export const useTypeDefs = gql`
 
   type User {
     id:ID!
-    name:String!,
-  }     
+    firstName:String!,
+    lastName:String!,
+    userName:String!,
+    indexRef:Int!,
+    createdAt:String
+  }
+  #posts needs create     
 `;
