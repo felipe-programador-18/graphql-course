5e24f97410f5fc33b680362693c0c6a75d68dc20
Felipe <machadofelipe2016@outlook.com>
2022-07-17 11:43:23 -0300

    c12-adding more date

--- src/graph/post/anothertypedefs.js
+++ src/graph/post/anothertypedefs.js
@@ -1,14 +1,29 @@
-import { ApolloServer, gql } from "apollo-server";
+import { gql } from "apollo-server";
 
 //create another typedefs simulating get another date!!
 
 export const useAnothertype = gql`
- extend type Query {
-   brand: newBrand
-  } 
-  type newBrand{
-   namebrand: String!
-   years: Int
-  }
-
+  extend  type Query {
+        post: Post!,
+        posts:[Post!]!  
+      }
+    
+    type Post {
+    id:ID!,
+    title: String!,
+    body:String!,
+    #userId: Int!,
+    indexRef: Int!,
+    createdAt:String!
+    }
 `;
+
+
+/*  {
+    "id": "860",
+    "title": "Et voluptatem nulla omnis et iusto ullam.",
+    "body": "Ad non pariatur. Aut molestias accusamus et inventore sunt voluptates non doloremque illum. Perspiciatis et provident vel et fugiat dolores ut. Quos quibusdam impedit cupiditate quia at eaque quae.",
+    "userId": "29",
+    "indexRef": 10,
+    "createdAt": "2018-08-10T23:41:51.714Z"
+  }, */
