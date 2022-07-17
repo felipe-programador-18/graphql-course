80bea07106af2f4f72e872a5a969f7f60040b453
Felipe <machadofelipe2016@outlook.com>
2022-07-17 15:07:06 -0300

    c22-create context inside principle page

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
-import { ApolloServer,gql } from 'apollo-server'
+import { ApolloServer} from 'apollo-server'
 import { resolvers, typeDefs } from './graph/schema'
+import fetch from "node-fetch"
 // here i tick server apollo
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-
+  context: () => {
+    return {
+      fetch,
+     
+    }
+  }
 })
 
 server.listen(4003).then(({url}) => {
