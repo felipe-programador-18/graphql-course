500ca33492fbf40176b07542ac064f766e2fba02
Felipe <machadofelipe2016@outlook.com>
2022-07-17 15:08:48 -0300

    c24-create more variable to simply date users

--- src/graph/user/resolvers.js
+++ src/graph/user/resolvers.js
@@ -1,13 +1,16 @@
-import fetch from "node-fetch"
 
-const  users = async () => {
+
+const  users = async(_, __, {fetch}) => { 
   const users = await fetch("http://localhost:3000/users")
-    return users.json();
+  return users.json();
 }
 
-const user = async () => {
-    const user = await fetch("http://localhost:3000/users")
-    return user.json()
+const user = async (_, {id}, { fetch }) => {
+  console.log(fetch)
+  const response = await fetch("http://localhost:3000/users/" + id)
+  const user = await response.json()
+  //console.log(user)
+  return user;
 }
 
 
