5ac33f04e8904d0f635dd6f8cb75baf69133625d
Felipe <machadofelipe2016@outlook.com>
2022-08-19 13:27:36 -0300

    c36-simpliplay all context

--- src/graph/context.js
+++ src/graph/context.js
@@ -1,10 +1,12 @@
 import fetch from "node-fetch"
+
+const Api_Url = "http://localhost:3000"
+
+// solve types of posts in graphql!!
+// this is exercise about graphql!!
 export const context = () => {
     return {
-      getUsers: (path = '/') => { 
-       const url = "http://localhost:3000/users" + path
-       console.log("get users called path", path,url)
-       return  fetch(url);
-    },
+      getUsers: (path = '/') => fetch (Api_Url + "/users" + path),
+      getPosts: (path = '/') => fetch(Api_Url +"/posts" +path)
     }
   }
